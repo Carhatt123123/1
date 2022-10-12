722831c130622e42f7042db34808365d9638e822
Carhatt123123 <114424398+Carhatt123123@users.noreply.github.com>
2022-10-12 18:37:03 +0300

    Add files via upload

---
+++ kondrashenok.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	/*int n, k;
+	cout << "n:";
+	cin >> n;
+	cout << "K:";
+	cin >> k ;
+	for (int i = 0; i < 0; ++i) {
+		cout << k << endl;
+
+	}*/
+	/*int a, b;
+	int n = 0;
+	cout << "a: ";
+	cin >> a;
+	cout << "b: ";
+	cin >> b;
+	for (int i = a; i <= b; i = i + 1) {
+		cout << i << endl;
+		n++;
+	}
+	cout << "qty = " << n << endl;*/
+	/*int a, b;
+	int n = 0;
+	cout << "a: ";
+	cin >> a;
+	cout << "b: ";
+	cin >> b;
+	for (int i = b-1; i > a; i = i - 1) {
+		cout << i << endl;
+		n++;
+	}
+	cout << "qty = " << n << endl;*/
+	/*double price;
+	cout << "input price ";
+	cin >> price;
+	for(int weight = 1; weight <= 10; weight++) {
+		cout << weight << " kg costs " << weight * price << " $" << endl;*/
+	//for 5
+	/*int n = 0;
+	double price;
+	cout << "задайте прайс";
+	cin >> price;
+	for (double weight = 0.1; weight < 1.0; weight += 0.1) {
+		cout << weight <<"$"<< weight * price << endl;
+		//for 6
+	}*/
+	/*int n = 0;
+	double price;
+	cout << "задайте прайс";
+	cin >> price;
+	for (double weight = 1.2; weight < 2.0; weight += 0.2) {
+		cout << weight << "$" << weight * price << endl;
+	}*/
+	//for 7
+	/*int a, b;
+	cout << "«адайте переменную A";
+	cin >> a;
+	cout << "«адайте переменную B больше чем A";
+	cin >> b;
+	int s = 0;
+	for (int i = a; i <= b; i = i+1) {
+		s = s + i;
+		cout << s << endl;
+
+	}*/
+}
